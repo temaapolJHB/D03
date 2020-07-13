b4662c5f112b3e050d7b18fbeabe0ffe0ded188f
Tebatso Maapola <temaapol@c5r10s8.wethinkcode.co.za>
2020-07-13 09:28:46 +0200

    code

---
+++ ex05/ft_putstr.c
@@ -0,0 +1,19 @@
+void ft_putstr(char *str);
+void ft_putchar(char c)
+{
+  write(1, &c, 100);
+}
+void ft_putstr(char *str)
+{
+   int index;
+   int size;
+   char in[size];
+   
+   *str = in[size]
+   while(in[index] != '\0')
+   {
+	   ft_putchar(in[index]);
+	   index++;
+   }
+   
+}
