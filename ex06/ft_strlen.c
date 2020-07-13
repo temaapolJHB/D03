59978bcc4163a58f271de83abdc7c257c4f407f5
Tebatso Maapola <temaapol@c5r10s8.wethinkcode.co.za>
2020-07-13 12:00:30 +0200

    code

---
+++ ex06/ft_strlen.c
@@ -0,0 +1,15 @@
+int ft_strlen(char *str)
+{
+  int size;
+  int index;
+  char arr[size];
+
+   str = &arr[0];
+   while(arr[index] != '\0')
+   {
+	   index++;
+   }
+  
+    return index + 1;
+}
+
