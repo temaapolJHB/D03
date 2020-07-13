1e40ffc1788404045d58abebf733779505301936
Tebatso Maapola <temaapol@c5r10s8.wethinkcode.co.za>
2020-07-13 11:44:23 +0200

    code

---
+++ ex01/ft_ultimate_ft.c
@@ -0,0 +1,29 @@
+void ft_utltimate_ft(int *********nbr);
+void ft_ultimate_ft(int **********nbr)
+{
+	int num;
+    int *********nr1;
+	int *******nr2;
+	int ******nr3;
+	int *****nbr4;
+	int ****nbr5;
+	int ***nbr6;
+	int **nbr7;
+	int *nbr8;
+
+	num = 42;
+    nbr8 = &num;
+	nbr8 = &nbr7;
+	nbr7 = &num6;
+	nbr6 = &num5;
+	num5 = &num4;
+	num4 = &num3;
+	num3 = &num2;
+	num2 = &num1;
+	nbr = & num1;
+
+}
+
+
+
+
