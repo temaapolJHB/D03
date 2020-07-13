6ad2a279e37ea2c44374c14a3fd55b5a445c290f
Tebatso Maapola <temaapol@c5r10s8.wethinkcode.co.za>
2020-07-13 08:59:22 +0200

    code

---
+++ ex04/ft_ultimate_div_mod.c
@@ -0,0 +1,14 @@
+void ft_ultimate_div_mod(int *a, int *b)
+{
+   int num1;
+   int num2;
+   int divTot;
+   int modTot;a
+
+   *a = &num1;
+   *b = &num2
+   divTot = num1 / num2;
+   modTot = num1 % num2;
+   *a = &divTot;
+   *b = &modTot;
+}
